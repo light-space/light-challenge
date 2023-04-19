5463a52255448f309d3a69a333cad0d783f8624d
Tiago Engel <tiagohngl@gmail.com>
2023-04-19 18:40:03 +0200

    update example

--- backend/challenge-app/src/main/kotlin/org/light/challenge/app/WorkflowResource.kt
+++ backend/challenge-app/src/main/kotlin/org/light/challenge/app/WorkflowResource.kt
@@ -1,7 +1,7 @@
 package org.light.challenge.app
 
 import javax.ws.rs.Consumes
-import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
@@ -11,8 +11,8 @@ import javax.ws.rs.core.MediaType
 @Consumes(MediaType.APPLICATION_JSON)
 class WorkflowResource {
 
-    @GET
-    fun getWorkflow() {
+    @POST
+    fun executeWorkflow() {
         throw NotImplementedError()
     }
 }
