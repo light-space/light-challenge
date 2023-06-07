d5dc5cda6fa4cc02af76ce4c9f05deb793d88889
Tiago Engel <tiagohngl@gmail.com>
2023-06-07 10:41:57 +0200

    Add proxy

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,6 @@
+// NOTE: the frontend server provides a proxy to the backend on the /api route
+// see: next.config.js if you need to change this
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
